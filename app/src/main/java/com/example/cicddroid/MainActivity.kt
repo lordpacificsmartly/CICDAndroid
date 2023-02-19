e70e3a8b65ea5f06b5bbeec8e6bde3c28c87a74b
Jesse <jesseonoyeyan@yahoo.com>
2023-02-19 19:09:25 +0100

    App Center Setup

--- app/src/main/java/com/example/cicddroid/MainActivity.kt
+++ app/src/main/java/com/example/cicddroid/MainActivity.kt
@@ -5,11 +5,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "491c7c6a-fc98-4b51-b808-8a0fc9a667ca", Analytics::class.java, Crashes::class.java)
 
         val nameEditText = findViewById<EditText>(R.id.nameTextView)
         val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
