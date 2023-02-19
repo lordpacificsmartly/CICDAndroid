9293908cf5bcc96468b94b6a6c1634b0f69abf26
Jesse <jesseonoyeyan@yahoo.com>
2023-02-19 11:51:24 +0100

    Add feature 1

--- app/src/main/java/com/example/cicddroid/MainActivity.kt
+++ app/src/main/java/com/example/cicddroid/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.cicddroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nameEditText = findViewById<EditText>(R.id.nameTextView)
+        val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
+        val helloButton = findViewById<Button>(R.id.helloBUtton)
+        helloButton.setOnClickListener{
+            "Hello${nameEditText.text}".also { greetingTextView.text = it }
+        }
     }
 }
